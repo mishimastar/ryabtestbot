fdd559ffbe8200e6a50e0ee0a729f94760d1be0d
Sergey Saltykov <s.saltykov@group-ib.com>
2023-07-20 15:07:23 +0700

    +

--- src/banks.ts
+++ src/banks.ts
@@ -1,4 +1,4 @@
-export const Banks = new Map<string, { percent: number; min: number; rateName: 'RSrub2baht' | 'PBrub2baht' }>([
+export const Banks = new Map<string, { percent: number; min: number; rateName: 'RSrub2baht' }>([
     ['РУССКИЙ СТАНДАРТ БАНК', { min: 100, percent: 0.02, rateName: 'RSrub2baht' }]
     // ['ПОЧТА БАНК', { min: 300, percent: 0.01, rateName: 'PBrub2baht' }]
     // ['ГАЗПРОМ БАНК', { min: 200, percent: 0.015, rateName: 'GPrub2baht' }],
